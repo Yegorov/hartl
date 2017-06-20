9de2bd001566d983d9a08f63e3dda994e5c4f1bd
Yegorov <yegorov0725@yandex.ru>
2017-06-20 19:15:37 +0300

    Add setup to test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  setup do
+    @main_title = "| RoR App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | RoR App"
+    assert_select "title", "Home #{@main_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | RoR App"
+    assert_select "title", "Help #{@main_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | RoR App"
+    assert_select "title", "About #{@main_title}"
   end
 end
