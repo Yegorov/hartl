4d272e6fda3a5d61234ab2e2b130ad24cf85430f
Yegorov <yegorov0725@yandex.ru>
2017-06-22 18:22:09 +0300

    Make uniq email for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   has_many :microposts
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
-                                    format: { with: /\A[A-Za-z0-9\.+\-\_]+@[A-Za-z\.]+[^\.\_]\z/ }
+                                    format: { with: /\A[A-Za-z0-9\.+\-\_]+@[A-Za-z\.]+[^\.\_]\z/ },
+                                    uniqueness: { case_sensitive: false }
 
 
   def posts
