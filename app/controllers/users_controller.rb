be8e3e4cac7d20c836ef984cc0a46fb3f2c227f7
Yegorov <yegorov0725@yandex.ru>
2017-08-14 12:56:15 +0300

    Fix test, add test case for valid user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
     # end
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Добро пожаловать!"
       redirect_to @user
     else
