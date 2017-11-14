c70f2763ba4ff2b8ea4260fd5bcf8d8c15be7a78
Yegorov <yegorov0725@yandex.ru>
2017-11-14 11:24:01 +0300

    Add UserMailer objects with tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,9 +46,12 @@ class UsersController < ApplicationController
     # end
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Добро пожаловать!"
-      redirect_to @user
+      # log_in @user
+      # flash[:success] = "Добро пожаловать!"
+      # redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
