5d1fabb0bb7889df30fff73cd866262476bd70c7
Yegorov <yegorov0725@yandex.ru>
2017-11-17 17:06:54 +0300

    Make destroy micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: [:destroy]
   before_action :set_micropost, only: [:show, :edit, :update, :destroy]
 
   # GET /microposts
@@ -63,11 +64,14 @@ class MicropostsController < ApplicationController
   # DELETE /microposts/1
   # DELETE /microposts/1.json
   def destroy
+    # @micropost.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
     @micropost.destroy
-    respond_to do |format|
-      format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
@@ -79,5 +83,10 @@ class MicropostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
