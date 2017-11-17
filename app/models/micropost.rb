fc70d8ac0e5634a232c46d6d79dc047ded4e60c4
Yegorov <yegorov0725@yandex.ru>
2017-11-17 19:38:29 +0300

    Add validates picture to microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,9 +5,17 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, length: { maximum: 140 },
                       presence: true
+  validate :picture_size
 
   def as_json(options = {})
     options[:except] ||= [:id]
     super(options)
+  end
+
+  private
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5 MB")
+    end
   end
 end
